9a9f4579aee79a70e1226b7e0fe7559dd896facd
skublin <szymonkublin@gmail.com>
2022-03-16 22:57:33 +0100

    new folder structure

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@
 class Pydom:
     def __init__(self):
         self.name = "Pydom World"
+        self.version = "0.0.1"
 
 
 if __name__ == "__main__":
     p = Pydom()
-    print(p.name)
-
+    print(p.name, p.version)
